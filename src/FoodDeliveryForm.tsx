aeb2550e22f58030160e9a07da8d46f488910fdd
yishai <yishai5555@gmail.com>
2025-03-06 11:31:22 +0200

    04-handle-events

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 
 type FoodDeliveryFormType = {
   customerName: string;
@@ -11,16 +11,46 @@ export default function FoodDeliveryForm() {
     mobile: '',
   });
 
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    console.log(values);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="form-floating mb-3">
         <input
           type="text"
+          name="customerName"
           className="form-control"
           placeholder="Customer Name"
+          value={values.customerName}
+          onChange={handleInputChange}
         />
         <label htmlFor="floatingInput">Customer Name</label>
       </div>
+
+      <div className="form-floating mb-3">
+        <input
+          type="text"
+          name="mobile"
+          className="form-control"
+          placeholder="Mobile"
+          value={values.mobile}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="floatingInput">Mobile</label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 }
