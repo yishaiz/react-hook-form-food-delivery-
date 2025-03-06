42d95a968a339ae3a39501e02abd7a70ca116da0
yishai <yishai5555@gmail.com>
2025-03-06 14:16:33 +0200

    08-rhf

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -5,59 +5,12 @@ type FoodDeliveryFormType = {
   customerName: string;
   mobile: string;
 };
-/*
-type FoodDeliveryFormErrorType = {
-  customerName: string;
-  mobile: string;
-};
-*/
 
 export default function FoodDeliveryForm() {
   const { register } = useForm<FoodDeliveryFormType>();
 
-  /*
-  const [values, setValues] = useState<FoodDeliveryFormType>({
-    customerName: '',
-    mobile: '',
-  });
-
-  const [errors, setErrors] = useState<FoodDeliveryFormErrorType>({
-    customerName: '',
-    mobile: '',
-  });
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    setValues({ ...values, [name]: value });
-  };
-
-
-  const validateFormData = (): boolean => {
-    // const errors: FoodDeliveryFormErrorType = {
-
-    const tempErrors: FoodDeliveryFormErrorType = {
-      customerName: '',
-      mobile: '',
-    };
-
-    if (values.customerName === '') {
-      tempErrors.customerName = 'Customer name is required';
-    }
-
-    if (values.mobile === '') {
-      tempErrors.customerName = 'Mobile is required';
-    }
-
-    setErrors(tempErrors);
-
-    return Object.values(tempErrors).every((x) => x === '');
-  };
-*/
-
   const onSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
     // e.preventDefault();
-
     // if (validateFormData()) {
     //   console.log('form data', values);
     // } else {
@@ -68,29 +21,21 @@ export default function FoodDeliveryForm() {
   return (
     <form autoComplete="off" onSubmit={onSubmit}>
       <div className="form-floating mb-3">
-      <input
-        type="text"
-        // name="customerName"
-        className="form-control"
-        placeholder="Customer Name"
-        // value={values.customerName}
-        // onChange={handleInputChange}
-        {register('customerName')}
-      />
-
-
-
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Customer Name"
+          {...register('customerName')}
+        />
         <label htmlFor="floatingInput">Customer Name</label>
       </div>
 
       <div className="form-floating mb-3">
         <input
           type="text"
-          name="mobile"
           className="form-control"
           placeholder="Mobile"
-          value={values.mobile}
-          onChange={handleInputChange}
+          {...register('mobile')}
         />
         <label htmlFor="floatingInput">Mobile</label>
       </div>
