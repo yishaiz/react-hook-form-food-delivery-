66e58689cee5f3422035c93867246e77b27feb95
yishai <yishai5555@gmail.com>
2025-03-17 17:57:22 +0200

    12-default-values

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,50 +1,82 @@
 import { useForm } from 'react-hook-form';
+import { useRenderCount } from './userRenderCount';
 
 type FoodDeliveryFormType = {
+  orderNo: number;
   customerName: string;
   mobile: string;
+  email: string;
 };
 
 export default function FoodDeliveryForm() {
-  const { register, handleSubmit } = useForm<FoodDeliveryFormType>();
-  // console.log(register('customerName'));
-
-  const onSubmit = (formData:FoodDeliveryFormType ) => {
-    
-  };
-
-  /*
-  
+  // useRenderCount()
+  const { register, handleSubmit } = useForm<FoodDeliveryFormType>({
+    defaultValues: {
+      orderNo: new Date().valueOf(),
+      customerName: '',
+      mobile: '',
+      email: '',
+    },
+  });
 
   const onSubmit = (formData: FoodDeliveryFormType) => {
-    console.log('form data', formData);
+    console.log({ formData });
   };
 
-  const onError = (errors) => {
-    console.log('validation errors', errors);
-  };
-*/
   return (
     <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-floating mb-3">
-        <input
-          {...register('customerName', { value: 'Fiona' })}
-          type="text"
-          className="form-control"
-          placeholder="Customer Name"
-        />
-        <label htmlFor="floatingInput">Customer Name</label>
+      <div className="row mb-2">
+        <div className="col">
+          <div className="form-floating">
+            <input
+              type="text"
+              disabled
+              className="form-control"
+              placeholder="#OrderNo"
+              {...register('orderNo')}
+            />
+            <label htmlFor="#OrderNo">Customer Name</label>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="form-floating">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Mobile"
+              {...register('mobile')}
+            />
+            <label htmlFor="floatingInput">Mobile</label>
+          </div>
+        </div>
       </div>
 
-      <div className="form-floating mb-3">
-        <input
-          {...register('mobile')}
-          type="text"
-          className="form-control"
-          placeholder="Mobile"
-        />
-        <label htmlFor="floatingInput">Mobile</label>
+      <div className="row mb-4">
+        <div className="col">
+          <div className="form-floating">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Customer Name"
+              {...register('customerName')}
+            />
+            <label htmlFor="floatingInput">Customer Name</label>
+          </div>
+        </div>
+        <div className="col">
+          <div className="form-floating">
+            <input
+              {...register('email')}
+              type="text"
+              className="form-control"
+              placeholder="Email"
+            />
+            <label htmlFor="floatingInput">Email</label>
+          </div>
+        </div>
       </div>
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
